1e9ebc2d1d66e3b9c580f26ef959abf0a3013f88
helmioo <81010386+helmioo@users.noreply.github.com>
2021-10-13 19:29:10 +0300

    todo-tehtävä, virheilmoitukset

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+const URL = 'http://localhost/todo/index.php'
+
 function App() {
+
+  const [tasks, setTasks] = useState([])
+  const [task, setTask] = useState('')
+
+  useEffect(() => {
+    axios.get(URL)
+    .then((response) => {
+     // console.log(response.data);
+      setTasks(response.data)
+    }).catch(error => {
+      alert(error.response ? error.response.data.error : error)
+    })
+  }, [])
+
+  function save(e) {
+    e.preventDefault()
+    const json = JSON.stringify({description:task})
+    axios.post(URL + 'add.php', json,{
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+    .then((response) => {
+      setTasks(tasks => [...tasks,response.data])
+      setTask('')
+    }).catch (error => {
+      alert(error.response.data.error)
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h3>Todo list</h3>
+      <form onSubmit={save}>
+        <label>New task</label>
+        <input value={task} onChange={e => setTask(e.target.value)}></input>
+        <button>Save</button>
+
+      </form>
+     <ol>
+      {tasks?.map(task => (
+        <li key={task.id}>{task.description}</li>
+      ))}
+     </ol>
     </div>
   );
 }
